44a74ad818f42ff9a772a5bff739a996d94a449e
V-Yashasvi <vyashasvi1@gmail.com>
2025-01-28 16:06:38 +0530

    Connected to mongoDB and completed milestone 6

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,40 @@
 const express=require('express')
 const app=express()
-
+const mongoose=require('mongoose')
 const PORT=8080
+const dotenv=require('dotenv')
+dotenv.config()
+const mongourl=process.env.mongoURL
+const connection=mongoose.connect(mongourl);
+const user=require('./model/user.model');
+
+app.use(express.json())
 
 app.get('/ping',(req, res)=>{
     res.send('pong');
 })
 
-app.listen(PORT,()=>{
+app.post('/create',async(req, res)=>{
+    let {username, email, password}=req.body;
+    let payload={username,email,password}
+    console.log(payload)
+    try{
+        let new_user=new user(payload)
+        await new_user.save()
+        res.send({"message":"Hurray! New user saved to Database successfully"})
+    }catch(error){
+        console.log(error)
+        res.send({"message":"Could'nt save the new user to database",error:error.message})
+    }
+})
+
+app.listen(PORT,async()=>{
+    try {
+        await connection;
+        console.log("Connected to mongoDB atlas")
+    } catch (error) {
+        console.log(err)
+    }
+    
     console.log(`Server is running on http://localhost:${PORT}`);
 })
