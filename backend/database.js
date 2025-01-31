7b695b495f04c24cffb79d8f75a5cb515c4e5132
V-Yashasvi <vyashasvi1@gmail.com>
2025-01-31 14:23:10 +0530

    Done with CRUD operations

---
+++ backend/database.js
@@ -0,0 +1,15 @@
+const mongoose=require('mongoose')
+const dotenv=require('dotenv')
+dotenv.config()
+const mongourl=process.env.mongoURL
+
+const connection=async()=>{
+    try {
+        await mongoose.connect(mongourl,{useNewUrlParser: true,useUnifiedTopology: true,})
+        console.log("Connected to mongoDB atlas")
+    } catch (error) {
+        console.log(err)
+    }
+}
+
+module.exports={connection}
